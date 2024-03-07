c1c5204a2bbd0a17a6da33bb4108e8c18d8c2380
arlohb <arlo@urwick.co.uk>
2024-03-07 20:24:28 +0000

    Made timer name an arg of change_timer

--- scripts/change_timer.sh
+++ scripts/change_timer.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+timer=$1
+new_time=$2
+
 # Make override folder
-mkdir -p ~/.config/systemd/user/nextcloud-sync.timer.d
+mkdir -p "~/.config/systemd/user/${timer}.timer.d"
 
 # Make override file
-echo -e "[Timer]\nOnUnitActiveSec=$1" \
-    > ~/.config/systemd/user/nextcloud-sync.timer.d/override.conf
+echo -e "[Timer]\nOnUnitActiveSec=${new_time}" \
+    > ~/.config/systemd/user/${timer}.timer.d/override.conf
 
 # Load changes
 systemctl --user daemon-reload
