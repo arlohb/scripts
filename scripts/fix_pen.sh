a5009e45bae578d1c66b1c77fecf73c65b414849
arlohb <arlo@urwick.co.uk>
2024-03-07 11:51:16 +0000

    fix_pen now has separate up and down events

--- scripts/fix_pen.sh
+++ scripts/fix_pen.sh
@@ -2,11 +2,23 @@
 
 pkill ydotoold
 ydotoold &
+
+# ydotool info
+# Uses a bitmask
+# 0x00 left
+# 0x40 down
+# 0x80 up
+# Can combine down and up to click
  
 libinput debug-events | while read -r event; do
-    echo $event | grep "event17.*TABLET_TOOL_TIP.*down" > /dev/null
+    echo $event | grep ".*TABLET_TOOL_TIP.*down" > /dev/null
     if [[ $? == 0 ]]; then
-        ydotool click 0xC0
+        ydotool click 0x40
+    fi
+
+    echo $event | grep ".*TABLET_TOOL_TIP.*up" > /dev/null
+    if [[ $? == 0 ]]; then
+        ydotool click 0x80
     fi
 done
 
